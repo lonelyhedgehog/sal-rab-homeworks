416deef6c036005f64d891444e73633d4ad41f68
Vera A <beemyvi@mail.ru>
2021-06-25 19:03:14 +0300

    Add files via upload
    task1

--- task_1/solution.js
+++ task_1/solution.js
@@ -13,7 +13,9 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message текст 'теперь в корзине!'
     // выведите переменную message в консоль
-
+    let message; 
+    message = productName + ' ' + 'за' + ' ' + productPrice + ' ' + 'теперь в корзине!';
+    console.log(message);
     // Конец решения задания №1.1.
 
     return message;
@@ -27,7 +29,9 @@ function addInCartCountChange(value) {
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
-
+    let newValue; 
+    newValue = oldValue;
+    newValue = newValue +1;
     // Конец решения задания №1.2.
 
     return newValue;
@@ -42,7 +46,8 @@ function addInCartSumChange(sum, delta) {
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
-
+    let newSum = oldSum + difference;
+    let newSumText = `${newSum} ₽`;
     // Конец решения задания №1.3.
 
     return newSumText;
