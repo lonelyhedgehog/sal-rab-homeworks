72df4087144d1b93d3585dd5ce43cd34dacf82f5
Vera A <beemyvi@mail.ru>
2021-06-26 13:06:56 +0300

    3

--- task_3/solution.js
+++ task_3/solution.js
@@ -1,74 +1,32 @@
-function calcShipping(sum, min, shipping) {
-    let productsSum = sum; // сумма в корзине
-    let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
-    let shippingPrice = shipping; // стоимость доставки
+function renderCartItem(item) {
+    const product = item;
 
-    // Задание №2.1. Рассчитать доставку
+    // Задание №3.1. Формирование строки корзины
 
-    // создайте переменную shippingSum
-    let shippingSum
-    // если productsSum равно 0,
-    // то shippingSum присвоить значение 0
-    if (productsSum == 0) {
-        shippingSum = 0;
-    } else if (productsSum >= freeShippingMinSum){
-        shippingSum = 0;
-    }
-    else if (productsSum > 0 && productsSum < freeShippingMinSum) {
-        shippingSum = shippingPrice;
-    }
-    // если productsSum Больше или равна freeShippingMinSum,
-    // то shippingSum присвоить значение 0
+    // product - объект вида {id: id, title: title, count: count, price: price}
+    // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
+    // id - идентификатор
+    // title - наименование
+    // price - цена
+    // count - количество
 
-    // если productsSum больше 0 и меньше freeShippingMinSum,
-    // то shippingSum присвоить значение shippingPrice
+    // сформируйте строку itemCountText вида:
+    // количество × цена ₽ = стоимость ₽
+    // например,
+    // 5 × 500.00 ₽ = 2500 ₽
+    // 1. Создайте переменную itemCountText, равную пустой строке
+    // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
+    // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
+    // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
+    // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
+    // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
+    // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
+    // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
+    let itemCountText;
+    let sum = product.price * product.count;
+    itemCountText = product.count +  ' × ' + product.price + ' ₽ = ' + sum + ' ₽';
+    // Конец решения задания №3.1.
+    // Конец решения задания №3.1.
 
-    // Конец решения задания №2.1.
-
-    return shippingSum;
-}
-
-function calcDiscount(sum, min, discount) {
-    let productsSum = sum; // сумма в корзине
-    let discountMinSum = min; // минимальная цена для скидки
-    let discountPart = discount; // величина скидки в процентах
-
-    // Задание №2.2. Рассчитать скидку
-
-    // создайте переменную discountPrice
-    let discountPrice;
-    // если productsSum больше или равно discountMinSum,
-    // то присвойте discountPrice значение discountPart процентов от productsSum,
-    // иначе присвойте discountPrice значения 0
-    if (productsSum >= discountMinSum) {
-        discountPrice = (productsSum*discountPart/100);
-    } else {discountPrice = 0;}
-    // Конец решения задания №2.2.
-
-    return discountPrice;
-}
-
-function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shippingPrice}) {
-    let productsSum = sum;
-    let discountSum = calcDiscount(sum, discountMinSum, discountPart);
-
-    // Задача №2.3. Рассчитать скидки и доставку в корзине
-
-    // создайте переменную totalSum
-    let totalSum;
-    // присвойте totalSum значение productsSum
-    // уменьшите totalSum на discountSum
-    totalSum = productsSum - discountSum;
-    let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
-
-    // прибавьте к totalSum значение shippingSum
-    totalSum = totalSum + shippingSum;
-    // создайте переменную freeShipping
-    let freeShipping;
-    // запишите без использования if или любых других условий:
-    // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
-    freeShipping = shippingSum == 0;
-    // Конец решения задачи №2.3.
-
-    return {discount: discountSum, freeShipping, shipping: shippingSum, total: totalSum};
+    return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
 }
