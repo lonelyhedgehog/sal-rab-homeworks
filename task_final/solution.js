c28fd5d8a3190184260d5e8baaf33a9bcc7bf2be
Vera A <beemyvi@mail.ru>
2021-06-29 12:18:21 +0300

    uodate

--- task_final/solution.js
+++ task_final/solution.js
@@ -38,14 +38,15 @@ function sendRequest(name, phone, address, goods, sum) {
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i <= countOfGoods; i ++) {
+        goods.title = data.goods.push(goods[i].title);
+        goods.count = data.goods.push(goods[i].count);
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = `${address}`;
+    data.order.sum = sum;
 
-    data.client = 'Иван';
+    data.client = name;
 
     let jsonData = JSON.stringify(data);
 
