a09c276b37aaee79244b46e1b75476b0362625db
Vera A <beemyvi@mail.ru>
2021-07-12 14:44:28 +0300

    TASK FINAL DONE

--- task_final/solution.js
+++ task_final/solution.js
@@ -38,17 +38,17 @@ function sendRequest(name, phone, address, goods, sum) {
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i ++) {
-        goods[i].title.count = title.count;
+    for (let i = 0; i < countOfGoods; i ++) {
+        goods[i] = {title: goods[i].title , count: goods[i].count}
         data.goods.push(goods[i]);
     }
 
-    data.order.address = `${address.street}, ${address.house}, ${address.entrance}, ${address.floor}, ${address.flat} `;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
     data.order.sum = sum;
 
-    data.client = name + ' - ' + phone;
+    data.client = name + ' ' + phone;
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
